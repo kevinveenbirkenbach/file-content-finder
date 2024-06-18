e689f4ae9ddef35e10fdeb531d8684c14c8fd4a3
Kevin Veen-Birkenbach <kevin@veen.world>
2024-06-18 19:33:26 +0200

    removed pdfhandler bug

--- handlers/pdf_handler.py
+++ handlers/pdf_handler.py
@@ -1,6 +1,9 @@
 from .base_handler import BaseHandler
 
 class PDFHandler(BaseHandler):
+    def __init__(self, search_string, file_type, search_path, verbose, list_only, ignore_errors, binary_files=None):
+        super().__init__(search_string, file_type, search_path, verbose, list_only, True, binary_files)
+    
     def search(self):
         find_cmd = ['find', self.search_path, '-type', 'f', '-iname', self.file_type, '-print0']
         self.process_files_in_parallel(find_cmd, self.process_pdf)
